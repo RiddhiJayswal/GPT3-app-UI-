de3d45d7ff8107be6ce6f7cd1d0bff93e40a83a0
Riddhi jayswal <riddhijayswal2610@gmail.com>
2024-09-15 17:06:49 +0530

    final commit

---
+++ src/components/import..jsx
@@ -0,0 +1,13 @@
+import google from'../../assets/google.png';
+import google from'../../assets/slack.png';
+import google from'../../assets/atlassian.png';
+import google from'../../assets/dropbox.png';
+import google from'../../assets/shopify.png';
+
+export{
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+}
